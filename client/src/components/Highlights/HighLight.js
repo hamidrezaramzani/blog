aa8b6cd1f1c07a3a48dd63ff1a3d5620816ade39
hamidrezaramzani <45255299+hamidrezaramzani@users.noreply.github.com>
2021-10-31 18:59:54 +0330

    Create highlights component

---
+++ client/src/components/Highlights/HighLight.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function HighLight() {
+    return (
+        <div className="w-4/6 h-auto py-5 bg-teal-500 flex mx-auto ">
+            <div className="h-auto w-3/6 p-2 ">
+                <img src="https://picsum.photos/615/640" alt="" />
+                <span className="w-full text-gray-400 py-3 block">2021/28/15 10:28</span>
+                <h3 className="text-2xl">This is first post that i create</h3>
+                <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, quos. Voluptate delectus pariatur ipsam, soluta ut</p>
+            </div>
+            <div className="w-3/6 h-auto flex flex-wrap">
+                <div className="w-1/2 h-auto p-2">
+                    <img src="https://picsum.photos/615/500" alt="" className="object-cover" />
+                    <span className="w-full text-gray-400 py-3 block">2021/28/15 10:28</span>
+                    <h3 className="text-xl">This is first post that i create</h3>
+                    <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, quos. Voluptate delectus pariatur ipsam, soluta ut</p>
+                </div>
+                <div className="w-1/2 h-auto p-2">
+                    <img src="https://picsum.photos/615/500" alt="" className="object-cover" />
+                    <span className="w-full text-gray-400 py-3 block">2021/28/15 10:28</span>
+                    <h3 className="text-xl">This is first post that i create</h3>
+                    <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, quos. Voluptate delectus pariatur ipsam, soluta ut</p>
+                </div>
+                <div className="w-1/2 h-auto p-2">
+                    <img src="https://picsum.photos/615/500" alt="" className="object-cover" />
+                    <span className="w-full text-gray-400 py-3 block">2021/28/15 10:28</span>
+                    <h3 className="text-xl">This is first post that i create</h3>
+                    <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, quos. Voluptate delectus pariatur ipsam, soluta ut</p>
+                </div>
+                <div className="w-1/2 h-auto p-2">
+                    <img src="https://picsum.photos/615/500" alt="" className="object-cover" />
+                    <span className="w-full text-gray-400 py-3 block">2021/28/15 10:28</span>
+                    <h3 className="text-xl">This is first post that i create</h3>
+                    <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, quos. Voluptate delectus pariatur ipsam, soluta ut</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default HighLight
